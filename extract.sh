349a6e13cfc357d4f389f616002f351169c0d3f7
Kellen Fox <Kellen.Fox@wwu.edu>
2015-02-21 15:48:37 -0800

    touchups

--- extract.sh
+++ extract.sh
@@ -1,32 +1,35 @@
 #!/usr/bin/env bash
 VERSION_FILE=~/.dotversion
+INTERNAL_VERSION_FILE=~/.dotfiles/version
+PKG_PATH=~/.dotfiles/pkgs
 
 get_installed_version() {
+    local line
     read line < $VERSION_FILE
-    return $line
+    echo $line
 }
 
 get_pkg_version() {
-    read line < ~/.dotfiles/version
-    return $line
+    local line
+    read line < $INTERNAL_VERSION_FILE
+    echo $line
 }
 
 do_install() {
-    # Start with vim items
-    ln -is ~/.dotfiles/vim/vimrc ~/.vimrc
-    ln -is ~/.dotfiles/vim/vim ~/.vim
-    # Do bash
-    ln -is ~/.dotfiles/bashrc ~/.bashrc
 
-    cp ~/.dotfiles/version $VERSION_FILE
+    for pkg in `ls $PKG_PATH`; do
+        for file in `ls $PKG_PATH/$pkg`; do
+            ln -is $PKG_PATH/$pkg/$file ~/.$file
+        done
+    done
+
+    cp $INTERNAL_VERSION_FILE $VERSION_FILE
 }
 
-if [ -f "$VERSION_FILE" ]; then
-    if [[ "$(get_pkg_version)" == "$(get_installed_version)" ]]
-    then
-        echo "Dotfiles are already at the newest version!"
-        exit 1
-    fi
+if [ -f "$VERSION_FILE" ] && [ `get_pkg_version` -eq `get_installed_version`]; then
+    echo "Dotfiles are already at the newest version!"
+    exit 1
 else
+    echo "Installing Dotfiles"
     do_install
 fi
