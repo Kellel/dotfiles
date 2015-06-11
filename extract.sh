ff65e7d457887b15d357a08c965911a6c34f5e52
Kellen Fox <kellen@cablespeed.com>
2015-06-11 14:16:34 -0700

    Cleaned up and added cool new functionality
    I removed all the neobundle bundles so that bundles will auto install on extract
    
    Added INSTALLHOOK functionality to extract script. you can now create installhooks for each package that run on setup

--- extract.sh
+++ extract.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 VERSION_FILE=~/.dotversion
 INTERNAL_VERSION_FILE=~/.dotfiles/version
+INSTALLHOOKFILE=INSTALLHOOK
 PKG_PATH=~/.dotfiles/pkgs
+BASH=`which bash`
 
 get_installed_version() {
     local line
@@ -19,14 +21,19 @@ do_install() {
 
     for pkg in `ls $PKG_PATH`; do
         for file in `ls $PKG_PATH/$pkg`; do
-            ln -is $PKG_PATH/$pkg/$file ~/.$file
+            if [ "$file" = "$INSTALLHOOKFILE" ]; then
+                echo "$PKG_PATH/$pkg/$file is an install-hook execing"
+                $BASH $PKG_PATH/$pkg/$file
+            else
+                ln -Tis $PKG_PATH/$pkg/$file ~/.$file
+            fi
         done
     done
 
     cp $INTERNAL_VERSION_FILE $VERSION_FILE
 }
 
-if [ -f "$VERSION_FILE" ] && [ `get_pkg_version` -eq `get_installed_version`]; then
+if [ -f "$VERSION_FILE" ] && [ `get_pkg_version` == `get_installed_version` ]; then
     echo "Dotfiles are already at the newest version!"
     exit 1
 else
